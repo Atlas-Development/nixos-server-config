2cf0b0baa1e17995f8a60e4f3e55e734adc7cd13
Xaver106 <xaver106@posteo.de>
2025-01-19 15:39:45 +0100

    add atlasmc-prod bootloader

--- hosts/atlasmc-prod/default.nix
+++ hosts/atlasmc-prod/default.nix
@@ -16,6 +16,9 @@
     networking.hostName = "atlasmc-prod";
     services.qemuGuest.enable = true;
 
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
     # services.minecraft-servers = {
     #   enable = true;
     #   eula = true;
