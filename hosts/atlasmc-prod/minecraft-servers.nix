8d2a5f5cf59e299352f07ca1fbfe84578a000076
Xaver106 <xaver106@posteo.de>
2025-01-19 20:20:15 +0100

    add atlasmc-prod mc servers

---
+++ hosts/atlasmc-prod/minecraft-servers.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  ...
+}:
+{
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+    servers = {
+
+      "survival" = {
+        enable = true;
+        package = pkgs.fabricServers.fabric-1_21_4;
+        jvmOpts = "-Xms6144M -Xmx6144M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20";
+      };
+
+      "velocity" = {
+        enable = true;
+        openFirewall = true;
+        package = pkgs.velocityServers-3_4_0-SNAPSHOT-build."461";
+        jvmOpts = "-Xms1024M -Xmx1024M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15";
+
+        stopCommand = "end";
+      };
+      
+    };
+  };
+}
