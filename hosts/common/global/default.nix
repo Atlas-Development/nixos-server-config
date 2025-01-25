532ccf657b357d106b1883b83eeeca4dbcd43d4b
Xaver106 <xaver106@posteo.de>
2025-01-25 12:08:40 +0100

    add some nix related settings

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -11,5 +11,15 @@
     environment.enableAllTerminfo = true;
     nixpkgs.config.allowUnfree = true;
     system.stateVersion = "24.11";
+
+    nix = {
+      settings = {
+        auto-optimise-store = true; # Try to reduce size of nix Store
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ]; # Enable Flakes and the nix Command
+      };
+    };
   };
 }
