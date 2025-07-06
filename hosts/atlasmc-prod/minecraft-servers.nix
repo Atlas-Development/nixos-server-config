d5b0d3f7f93c0dbcce3374cf66d27cb7a6ccd668
Xaver106 <xaver106@posteo.de>
2025-07-06 20:31:01 +0200

    chore: update lockfile and minecraft servers

--- hosts/atlasmc-prod/minecraft-servers.nix
+++ hosts/atlasmc-prod/minecraft-servers.nix
@@ -10,20 +10,14 @@
 
       "survival" = {
         enable = true;
-        package = pkgs.fabricServers.fabric-1_21_4;
-        jvmOpts = "-Xms3072M -Xmx3072M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20";
-      };
-
-      "creative" = {
-        enable = true;
-        package = pkgs.fabricServers.fabric-1_21_4;
+        package = pkgs.fabricServers.fabric-1_21_5;
         jvmOpts = "-Xms3072M -Xmx3072M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20";
       };
 
       "velocity" = {
         enable = true;
         openFirewall = true;
-        package = pkgs.velocityServers.velocity-3_4_0-SNAPSHOT-build_461;
+        package = pkgs.velocityServers.velocity-3_4_0-SNAPSHOT-build_521;
         jvmOpts = "-Xms1024M -Xmx1024M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15";
 
         stopCommand = "shutdown";
