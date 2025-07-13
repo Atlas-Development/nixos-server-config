9c690ce5eb4590d113ee768543eb5a8a1e8063ea
Xaver106 <xaver106@posteo.de>
2025-07-13 21:00:29 +0200

    chore(flake): update to 25.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
 
     # Official NixOS package sources
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
